9ec29ee7c5c39d8256975a827a1fb04c5cd05f5c
Matheus Araujo <matheusnewff@gmail.com>
2021-03-27 10:41:27 -0300

    Tela de Preload com imagem e ActivityIndicator estilizados

--- src/stacks/MainStack.js
+++ src/stacks/MainStack.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, HeaderTitle } from '@react-navigation/stack';
 
 import Preload from '../screens/Preload';
 import Signin from '../screens/Signin';
@@ -9,8 +9,13 @@ const stack = createStackNavigator();
 
 {/* Dentro desse componente ficaram as todas as telas dessa stack */};
 export default () =>(    
-    <stack.Navigator>
-        <stack.Screen name="Preload" component={Preload}/>
+    <stack.Navigator
+        initialRouteName="Preload"
+        screenOptions={{         
+            headerShown:false,
+        }}
+    >
+        <stack.Screen name="Preload" component={Preload} />
         <stack.Screen name="Signin" component={Signin}/>
         <stack.Screen name="Signup" component={Signup}/>
     </stack.Navigator>
