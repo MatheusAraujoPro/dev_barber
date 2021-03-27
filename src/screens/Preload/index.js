1d9bb1c164965b05e42fd39a6c13c981e1982539
Matheus Araujo <matheusnewff@gmail.com>
2021-03-27 11:24:03 -0300

    Usando Hooks para verificar e validar token e navegar para a pagina de signin

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Container, LoadingIcon } from './styles';
+import {useNavigation} from '@react-navigation/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import BarberLogo from '../../assets/barber.svg';
 
 
 export default () => {
+
+    const navigation = useNavigation();
+
+    //Hook que vai executar uma função sempre que chegar nesta tela
+    useEffect(()=>{
+        const checkToken =  async() => {
+            const token = await AsyncStorage.getItem('token');
+            if(token){
+                //Validar Token
+            }else{
+                //Navegue para a tela de login
+                navigation.navigate('Signin');
+            }
+        }
+        checkToken();
+
+    }, []);
+
     return (
         <Container>
            <BarberLogo width="100%" height="160"/>
